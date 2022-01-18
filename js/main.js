47c4aa1cf1979b17599a05f956edda16b4b51865
KettyPe <86797322+KettyPe@users.noreply.github.com>
2022-01-18 14:24:25 +0700

    Production

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        loop: true,
+        autoplay: true,
+        speed: 300,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+
+});
+
+AOS.init({
+    disable: 'tablet',
+    once: true,
+    duration: 800
+});
+
+AOS.init({
+    disable: function () {
+        var maxWidth = 992;
+        return window.innerWidth < maxWidth;
+    }
+});
